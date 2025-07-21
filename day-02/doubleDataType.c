1905991b3ddbcb8497891f8d6c92c73666e023f8
Ahsan Habib Hamim <youremail@example.com>
2025-07-22 02:50:05 +0600

    Double  Data Type in C

---
+++ day-02/doubleDataType.c
@@ -0,0 +1,14 @@
+/*
+Some double type variables name like num1, num2 etc. that will contain different double type number value
+--> Write a program that print all the double data type number !
+*/
+
+#include <stdio.h>
+int main()
+{
+    double num1 = 10.555555555555555;               // declare and initialize an double type variable
+    double num2 = 20.2199999999915;                 // declare and initialize an double type variable
+    printf("The Number 1 is %lf\n", num1);          // print the value of num1
+    printf("The Number 2 is %lf\n", num2);          // print the value of num2
+    printf("Number are %lf and %lf\n", num1, num2); // print the value of num1 and num2
+}
--- day-02/floatingDataType.c
+++ day-02/floatingDataType.c
@@ -6,8 +6,8 @@ Some floating variables name like num1, num2 etc. that will contain different fl
 #include <stdio.h>
 int main()
 {
-    float num1 = 10.5;                            // declare and initialize an integer variable
-    float num2 = 20.215;                          // declare and initialize an integer variable
+    float num1 = 10.5;                            // declare and initialize an float variable
+    float num2 = 20.215;                          // declare and initialize an float variable
     printf("The Number 1 is %f\n", num1);         // print the value of num1
     printf("The Number 2 is %f\n", num2);         // print the value of num2
     printf("Number are %f and %f\n", num1, num2); // print the value of num1 and num2
