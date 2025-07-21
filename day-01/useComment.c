bac2b71851c97e58605cc2d60adc3c85de2700d6
Ahsan Habib Hamim <youremail@example.com>
2025-07-22 01:18:41 +0600

    use comment in C code

---
+++ day-01/useComment.c
@@ -0,0 +1,13 @@
+/*
+Name: Ahsan Habib Hamim
+Email: coderahsan3.0@gmail.com
+Phone: +8801993899080
+Codding Time: 01.16 AM
+Location: Firmgate, Dhaka-1208, Bangladesh
+*/
+#include <stdio.h> // This is a header file for input/output operations
+int main() // This is the main function where the program starts execution
+{
+    printf("This is Ahsan Habib Hamim"); // This is a function to print the string
+    return 0; // This is a return statement to indicate the end of the program
+}
