616a36e2ddb10583b688847736e2bb96f3847594
Ahsan Habib Hamim <youremail@example.com>
2025-07-22 01:35:11 +0600

    use Escape Sequence for creating new line

---
+++ day-01/escapeSequence.c
@@ -0,0 +1,19 @@
+/*
+Make a program that print the following details in several new line :-
+--> Name: Ahsan Habib Hamim
+--> Email: coderahsan3.0@gmail.com
+--> Phone: +8801993899080
+--> Address: Dhaka, Bangladesh
+--> GitHub Username: AhsanHabibHamim
+*** Main Theme is use Escape Sequence Backslase(\) for create a new Line ***
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Name: Ahsan Habib Hamim\n");
+    printf("Email: coderahsan3.0@gmail.com\n");
+    printf("Phone: +8801993899080");
+    printf("Address: Dhaka, Bangladesh\n");
+    printf("GitHub Username: AhsanHabibHamim");
+    return 0;
+}
