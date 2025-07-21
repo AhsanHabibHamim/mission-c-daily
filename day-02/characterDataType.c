afc354bbd258eefab3a819b4a30f28ea80906b4a
Ahsan Habib Hamim <youremail@example.com>
2025-07-22 02:57:01 +0600

    character  Data Type in C

---
+++ day-02/characterDataType.c
@@ -0,0 +1,14 @@
+/*
+Some char type variables name like ch1, ch2 etc. that will contain different char type number value
+--> Write a program that print all the char data type number !
+*/
+
+#include <stdio.h>
+int main()
+{
+    char ch1 = 'A';                             // declare and initialize an char type variable
+    char ch2 = 'M';                             // declare and initialize an char type variable
+    printf("The Name 1 is %c\n", ch1);          // print the value of num1
+    printf("The Name 2 is %c\n", ch2);          // print the value of num2
+    printf("Number are %c and %c\n", ch1, ch2); // print the value of num1 and num2
+}
