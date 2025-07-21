9478263f8b71cfd1457758ee16edf9f55a29923b
Ahsan Habib Hamim <youremail@example.com>
2025-07-22 02:45:14 +0600

    Floating  Data Type in C

---
+++ day-02/floatingDataType.c
@@ -0,0 +1,14 @@
+/*
+Some floating variables name like num1, num2 etc. that will contain different floating number value
+--> Write a program that print all the floating number !
+*/
+
+#include <stdio.h>
+int main()
+{
+    float num1 = 10.5;                            // declare and initialize an integer variable
+    float num2 = 20.215;                          // declare and initialize an integer variable
+    printf("The Number 1 is %f\n", num1);         // print the value of num1
+    printf("The Number 2 is %f\n", num2);         // print the value of num2
+    printf("Number are %f and %f\n", num1, num2); // print the value of num1 and num2
+}
--- day-02/integerDataType.c
+++ day-02/integerDataType.c
@@ -1,14 +1,14 @@
 /*
-Some intiger variables name like num1, num2 etc. that will contain different integer value 
+Some intiger variables name like num1, num2 etc. that will contain different integer value
 --> Write a program that print all the integer number !
 */
 
 #include <stdio.h>
 int main()
 {
-    int num1 = 10; // declare and initialize an integer variable
-    int num2 = 20; // declare and initialize an integer variable
-    printf("The Number 1 is %d\n", num1); // print the value of num1
-    printf("The Number 2 is %d\n", num2); // print the value of num2
+    int num1 = 10;                                // declare and initialize an integer variable
+    int num2 = 20;                                // declare and initialize an integer variable
+    printf("The Number 1 is %d\n", num1);         // print the value of num1
+    printf("The Number 2 is %d\n", num2);         // print the value of num2
     printf("Number are %d and %d\n", num1, num2); // print the value of num1 and num2
 }
