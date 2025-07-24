b1dd17688fb2b55e1380f81682f82579338e4856
Ahsan Habib Hamim <youremail@example.com>
2025-07-24 14:42:46 +0600

    sizeOf in c

---
+++ day-03/sizeof.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    char c;
+    float f;
+    double d;
+
+    printf("Size of integer %d \n", sizeof(n));
+    printf("Size of character %d \n", sizeof(c));
+    printf("Size of float %d \n", sizeof(f));
+    printf("Size of douuble %d \n", sizeof(d));
+
+    return 0;
+}
